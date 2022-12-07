55ade5ed1949922834a8a102cc31d85311585b1c
Firdavsy07 <firdavsmuqimtoshov@gmail.com>
2022-12-07 10:16:35 -0500

    js

---
+++ vite-project/js/array.js
@@ -0,0 +1,16 @@
+const steamGames = [
+  {
+    uuid: {
+      game: "Portal2",
+      price: "9.99 USD",
+      score: "98.78%",
+      rating: "Overwhelmingly Positive",
+    },
+    uuid: {
+      game: "Warhammer 40,000: Darktide",
+      price: "39.99 USD",
+      score: "98.78%",
+      rating: "Mixed",
+    },
+  },
+];
