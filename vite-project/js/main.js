6274d151da4c36e4169fd042a2a39f2c8d5b198f
Firdavsy07 <firdavsmuqimtoshov@gmail.com>
2022-12-07 10:01:40 -0500

    css

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,6 +1,18 @@
 const DOMselectors = {
   card: document.querySelector(".cardId"),
   title: document.querySelector("#title"),
+  btn: document.querySelector("#btn"),
 };
 
-DOMselectors.title.insertAdjacentHTML();
+console.log(DOMselectors.btn);
+DOMselectors.btn.addEventListener("click", function () {
+  console.log("Success!");
+  if (document.body.classList.contains("dark-mode")) {
+    document.body.classList.add("light-mode");
+    document.body.classList.remove("dark-mode");
+  } else {
+    document.body.classList.contains("light-mode");
+    document.body.classList.add("dark-mode");
+    document.body.classList.remove("light-mode");
+  }
+});
